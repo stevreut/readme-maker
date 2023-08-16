e9b26f23d8584872a5ac4c077b4d0785f2666c00
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-08-15 22:37:39 -0400

    writeFile proof-of-concept is working and uses prompt input as content

--- index.js
+++ index.js
@@ -16,6 +16,7 @@
 // probably ultimately to be deleted.
 
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 console.log('preparing to prompt for info');
 
@@ -43,4 +44,26 @@ inquirer.prompt(questions).then((answers) => {
     tempAnswers = answers;
     answersJsonString = JSON.stringify(answers, null, '  ');
     console.log('JSON of answers = \n' + answersJsonString);
-});
+    return answers;
+}).then((data) => {
+    // TODO data is a place-holder name, ultimate TBD
+    console.log('data.description = "' + data.description + '"');
+    console.log('data.size = "' + data.size + '"');
+    const {description,size} = data;
+    let logContent = 
+`Demo README as log file
+================
+ Description:
+    ${description}
+================
+ Size:
+    ${size}
+================`;
+    fs.writeFile('./misc/log.txt',logContent, (err) => {
+        if (err) {
+            console.log('error on write = "' + err + '"');
+        } else {
+            console.log('write to ./misc/log.txt was successful');
+        }
+    })
+});  // TODO - no error checking?!
